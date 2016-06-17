c32907fe2bc74e4c9de539b1caa4a916feb350a5
Snehil Verma <romilvrm@gmail.com>
2016-06-18 00:31:03 +0530

    recycler view and share button done

--- app/src/main/java/com/example/mahe/shareplay/MainActivity.java
+++ app/src/main/java/com/example/mahe/shareplay/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.mahe.shareplay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -14,41 +18,62 @@ import java.util.Comparator;
 import android.net.Uri;
 import android.content.ContentResolver;
 import android.database.Cursor;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private ArrayList<Song> songList;
-    private ListView songView;
+    private RecyclerView songView;
+    Button button;
+    SongAdapter songAdt;
+
+
+    Song songs[];
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        songView = (ListView)findViewById(R.id.song_list);
+        songView = (RecyclerView) findViewById(R.id.song_list);
         songList = new ArrayList<Song>();
+
         getSongList();
 
 
-        Collections.sort(songList, new Comparator<Song>(){
-            public int compare(Song a, Song b){
+        Collections.sort(songList, new Comparator<Song>() {
+            public int compare(Song a, Song b) {
                 return a.getTitle().compareTo(b.getTitle());
             }
         });
 
-        SongAdapter songAdt = new SongAdapter(this, songList);
+        songAdt = new SongAdapter(songList);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        songView.setLayoutManager(mLayoutManager);
+        songView.setItemAnimator(new DefaultItemAnimator());
         songView.setAdapter(songAdt);
+
+
+        // button=(Button)findViewById(R.id.button);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageResource(R.drawable.icon_share);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                shareIt(view);
+
             }
         });
+
+
     }
 
     @Override
@@ -65,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
 
-        if(musicCursor!=null && musicCursor.moveToFirst()){
+        if (musicCursor != null && musicCursor.moveToFirst()) {
             //get columns
             int titleColumn = musicCursor.getColumnIndex
                     (android.provider.MediaStore.Audio.Media.TITLE);
@@ -83,4 +108,53 @@ public class MainActivity extends AppCompatActivity {
             while (musicCursor.moveToNext());
         }
     }
+
+
+
+    public void shareIt(View view) {
+        //sharing implementation here
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < songAdt.mCheckStates.size(); i++) {
+            if (songAdt.mCheckStates.get(i) == true) {
+
+                result.append(songList.get(i).getTitle());
+                result.append("\n");
+            }
+
+        }
+
+        displayToast(result);
+
+
+
+
+        /*
+        Snackbar.make(view, result, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+
+
+
+
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+
+        sharingIntent.setType("text/plain");
+        String shareBody = "enter text to send";
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+    }
+
+        */
+
+    }
+
+
+    public void displayToast(StringBuilder result){
+        Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+
+    }
 }
+
--- app/src/main/java/com/example/mahe/shareplay/SongAdapter.java
+++ app/src/main/java/com/example/mahe/shareplay/SongAdapter.java
@@ -1,10 +1,15 @@
 package com.example.mahe.shareplay;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,28 +18,88 @@ import java.util.ArrayList;
 /**
  * Created by MAHE on 6/17/2016.
  */
-public class SongAdapter extends BaseAdapter {
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.MyViewHolder> implements CompoundButton.OnCheckedChangeListener {
+
+    SparseBooleanArray mCheckStates;
 
 
     private ArrayList<Song> songs;
     private LayoutInflater songInf;
 
-    public SongAdapter(Context c, ArrayList<Song> theSongs){
-        songs=theSongs;
-        songInf=LayoutInflater.from(c);
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView title,artist;
+        public CheckBox checkBox;
+
+
+
+
+
+        public MyViewHolder(View view){
+            super(view);
+            title=(TextView)view.findViewById(R.id.song_title);
+            artist=(TextView)view.findViewById(R.id.song_artist);
+            checkBox=(CheckBox)view.findViewById(R.id.checkBox);
+
+
+        }
     }
 
-    public int getCount() {
+
+
+
+
+    public SongAdapter(ArrayList<Song> theSongs){
+        songs=theSongs;
+        //songInf=LayoutInflater.from(c);
+        mCheckStates=new SparseBooleanArray(theSongs.size());
+    }
+
+
+
+
+
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent,int viewType){
+        View itemView= LayoutInflater.from(parent.getContext()).inflate(R.layout.cards_layout,parent,false);
+
+        return new MyViewHolder(itemView);
+
+
+    }
+
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Song song=songs.get(position);
+
+        holder.title.setText(song.getTitle());
+        holder.artist.setText(song.getArtist());
+
+
+
+        holder.checkBox.setTag(position);
+        holder.checkBox.setChecked(mCheckStates.get(position,false));
+        holder.checkBox.setOnCheckedChangeListener(this);
+
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
         return songs.size();
     }
 
 
 
-    @Override
-    public Object getItem(int arg0) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+
+
+
 
     @Override
     public long getItemId(int arg0) {
@@ -42,22 +107,76 @@ public class SongAdapter extends BaseAdapter {
         return 0;
     }
 
+
+    public boolean isChecked(int position){
+        return mCheckStates.get(position,false);
+    }
+
+
+
+
+    public void setChecked(int position,boolean isChecked){
+        mCheckStates.put(position,isChecked);
+    }
+
+
+
+
+
+    public void toggle(int position){
+        setChecked(position,!isChecked(position));
+    }
+
+
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        mCheckStates.put((Integer)buttonView.getTag(),isChecked);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    /*
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         //map to song layout
-        LinearLayout songLay = (LinearLayout)songInf.inflate
-                (R.layout.song, parent, false);
+        LinearLayout songLay = (LinearLayout)songInf.inflate(R.layout.song, parent, false);
         //get title and artist views
         TextView songView = (TextView)songLay.findViewById(R.id.song_title);
         TextView artistView = (TextView)songLay.findViewById(R.id.song_artist);
+        CheckBox checkBox=(CheckBox)songLay.findViewById(R.id.checkBox);
         //get song using position
         Song currSong = songs.get(position);
         //get title and artist strings
+
         songView.setText(currSong.getTitle());
         artistView.setText(currSong.getArtist());
+
+        // checkbox handling//
+        checkBox.setTag(position);
+        checkBox.setChecked(mCheckStates.get(position,false));
+        checkBox.setOnCheckedChangeListener(this);
+
+
+
         //set position as tag
         songLay.setTag(position);
+
         return songLay;
     }
+
+    */
+
+
 
 }
